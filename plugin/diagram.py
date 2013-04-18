a331310ce7148673a92726dfe3175ceecde8f446
petrovic10 <lachimcivortep@gmail.com>
2013-04-18 22:28:45 +0000

    added base diagram import, new icon

--- plugin/diagram.py
+++ plugin/diagram.py
@@ -1,7 +1,12 @@
 #coding=utf-8
 class Diagram:
+    def __init__(self, pa_diagram=None, pa_parent_package=None,
+                 pa_parent_element=None, pa_type=None, pa_name=None):
+        self.name=pa_name
+        self.type=pa_type
+        self.connections=[]
+        self.elements=[]
 
-    def __init__(self):
-        self.name
-        self.type
-        self.connections
+        self.diagram_ID=pa_diagram
+        self.parent_package_ID=pa_parent_package
+        self.parent_ID=pa_parent_element
